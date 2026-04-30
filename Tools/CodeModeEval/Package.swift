7e230cef78bea079d9a7a2c3e19160d71bdbeb4e
Zac White <zac@velosmobile.com>
2026-04-29 21:03:47 -0700

    Stabilize eval package dependency identity

--- Tools/CodeModeEval/Package.swift
+++ Tools/CodeModeEval/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(path: "../.."),
+        .package(name: "codemode-ios", path: "../.."),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.7.0"),
         .package(url: "https://github.com/velos/wavelike-ios.git", branch: "main"),
         .package(url: "https://github.com/velos/CallableFunction.git", branch: "feature/function-updates"),
